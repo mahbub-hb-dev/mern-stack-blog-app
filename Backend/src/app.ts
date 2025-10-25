a0e47d8215d0ae2b186a6c112840a78e3e9a900d
Mahbub Hasan <mahbub.hb.dev@gmail.com>
2025-10-25 10:09:40 +0600

    1st class of Backend has completed

---
+++ Backend/src/app.ts
@@ -0,0 +1,15 @@
+import express, { type Application } from "express";
+import router from "./app/router/index.js";
+
+const app : Application = express()
+
+//Middleware 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello Belal!')
+})
+
+app.use('/api/v1', router);
+
+export default app;
---
+++ Backend/src/app/router/index.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { UserRoute } from "../modules/user/user.route.js";
+
+const router = Router();
+
+interface Iroute {
+    path : string;
+    route : Router;
+}
+
+const routeList : Iroute[] = [
+    {
+        path : "/user",
+        route : UserRoute,
+    }
+]
+
+
+routeList.forEach(route => {
+    router.use(route.path, route.route);
+})
+
+export default router;
